00b70290de813aeb337e5fa7715d4defeeecab3d
Hexosse <hexosse@gmail.com>
2018-05-24 13:49:06 +0200

    Don't override at startup

--- fs/groovy/100-executors.groovy
+++ fs/groovy/100-executors.groovy
@@ -3,7 +3,9 @@
 import jenkins.model.*
 import hudson.model.*
 
+// Environnement variables
 def env = System.getenv()
+// Jenkins instance
 def jenkins = Jenkins.getInstance()
 
 // Define the number of executors
